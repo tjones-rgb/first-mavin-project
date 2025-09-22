634b375b24099c53e32ecbe99329b5915b7f3625
Tjones <taouris.jones@gmail.com>
2025-09-22 15:31:08 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("butter eggs milk greens chicken oranges rice bread");
+    }
+}
